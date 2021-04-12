e2efd05f533798a75af9c6bd06d701e9525a06d0
Vishal <vishal021101995@gmail.com>
2021-04-12 16:34:03 +0530

    python solution for brans photos codeforces problem

--- Brain's Photos.py
+++ Brain's Photos.py
@@ -1,12 +1,11 @@
-# n, m = map(int, input().split())
-# flag = True
-# for i in range(n):
-#     row = input().split()
-#     if 'C' in row or 'M' in row or 'Y' in row:
-#         flag = True
-#         break
-#
-# if flag:
-#     print("#Color")
-# else:
-#     print("#Black&White")
+n, m = map(int, input().split())
+flag = False
+for i in range(n):
+    row = input().split()
+    if 'C' in row or 'M' in row or 'Y' in row:
+        flag = True
+
+if flag:
+    print("#Color")
+else:
+    print("#Black&White")
