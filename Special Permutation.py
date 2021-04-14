ae642d0d1fc65be7a66e68e9e5efae7bf3e482cc
Vishal <vishal021101995@gmail.com>
2021-04-14 21:02:21 +0530

    python solution for special permutation codeforces problem

--- Special Permutation.py
+++ Special Permutation.py
@@ -1,13 +1,13 @@
-# no_of_test_cases = int(input())
-# while no_of_test_cases:
-#     no_of_test_cases -= 1
-#     n = int(input())
-#     data = list(range(1, n+1))
-#     temp = data[0]
-#     data.pop(0)
-#     data.append(temp)
-#     res = ""
-#     for element in data:
-#         res += str(element) + " "
-#     print(res)
-#
+no_of_test_cases = int(input())
+while no_of_test_cases:
+    no_of_test_cases -= 1
+    n = int(input())
+    data = list(range(1, n+1))
+    temp = data[0]
+    data.pop(0)
+    data.append(temp)
+    res = ""
+    for element in data:
+        res += str(element) + " "
+    print(res)
+
