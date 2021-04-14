dde0c062c51f7fb07773d48ff5aab37f2ff99ab9
Vishal <vishal021101995@gmail.com>
2021-04-14 21:01:42 +0530

    python solution for fashionablee codeforces problem

--- FashionabLee.py
+++ FashionabLee.py
@@ -1,8 +1,8 @@
-# no_of_polygon = int(input())
-# while no_of_polygon > 0:
-#     no_of_polygon -= 1
-#     n = int(input())
-#     if n % 2 == 0:
-#         print("YES")
-#     else:
-#         print("NO")
+no_of_polygon = int(input())
+while no_of_polygon > 0:
+    no_of_polygon -= 1
+    n = int(input())
+    if n % 4 == 0:
+        print("YES")
+    else:
+        print("NO")
