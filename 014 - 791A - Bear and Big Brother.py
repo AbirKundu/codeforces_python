da9bfbacb119beac024aa74b7f0c7ba53b15b8e2
AbirKundu <115333620+AbirKundu@users.noreply.github.com>
2022-12-21 21:34:00 +0600

    Update 014 - 791A - Bear and Big Brother.py

--- 014 - 791A - Bear and Big Brother.py
+++ 014 - 791A - Bear and Big Brother.py
@@ -1,4 +1,4 @@
-limak, bob = input().split()
+limak, bob = input().split()    #  limak, bob = map(int,input().split()) <<<<<<<<<< instead of 1st 3 lines.
 limak = int(limak)
 bob = int(bob)
 year_count = 0
